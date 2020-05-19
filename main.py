f1072dadbd5f632f8b4d0f00aef64ae9c06768f2
Jestella <jestellaa@gmail.com>
2020-05-19 02:02:39 +0000

    added title() and upper()

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def translate(w):
   w = w.lower()
   if w in data:
       return data[w]
+  elif w.title() in data:
+    return data[w.title()]
+  elif w.upper() in data:
+    return data[w.upper()]
   elif len(get_close_matches(w, data.keys())) > 0:
       yn = input("Did you mean %s instead? Type Y for yes, N for no: " % get_close_matches(w,data.keys())[0])
       if yn == "Y" or "y":
